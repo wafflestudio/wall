3176cef3f99f4de10d166afac50453325a61a58b
gin <jineok12@gmail.com>
2012-05-16 16:48:56 +0900

    add bootstrap, about page, contact page(example)

--- infinitewall/app/controllers/Application.scala
+++ infinitewall/app/controllers/Application.scala
@@ -6,7 +6,16 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+  def about = Action {
+    Ok(views.html.about())
+  }
+  def contact = Action {
+    Ok(views.html.contact())
+  }
+  def hello(name:String) = Action {
+    Ok("Hello " + name)
   }
   
 }
