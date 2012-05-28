e04b099d28fc8bf8bafaed7b89a3ef12d19a43e7
Won-Wook Hong <jradoo@gmail.com>
2012-05-29 00:11:03 +0900

    added mailer and its conf

---
+++ infinitewall/app/utils/Mailer.scala
@@ -0,0 +1,43 @@
+package utils
+
+import play.api.Play.current
+import org.apache.commons.mail._
+import com.typesafe.config.ConfigFactory
+
+object Mailer {
+
+	lazy val config = ConfigFactory.load()
+	lazy val host = config.getString("smtp.host")
+	lazy val port = config.getInt("smtp.port")
+	lazy val ssl = config.getBoolean("smtp.ssl")
+	lazy val username = config.getString("smtp.user")
+	lazy val password = config.getString("smtp.password")
+	lazy val from = config.getString("smtp.from")
+
+	def send(subject: String, content: String, recipient: String) = {
+
+		var email: Email = new HtmlEmail();
+		email.setHostName(host);
+		email.setSmtpPort(port);
+		email.setAuthenticator(new DefaultAuthenticator(username, password));
+		email.setSSL(ssl);
+		
+		email.setFrom(from);
+		email.setSubject(subject);
+		email.setMsg(content);
+		email.addTo(recipient);
+		email.send();
+
+	}
+
+	//	def sendVerification(user: User) = {
+	//		send("Verify your account at Infinite wall", """
+	//			<html>
+	//				<p><a href="@URL"></a></p>
+	//			</html>
+	//			""".replaceAll("@URL", "http://localhost:9000/user/verify?value="),
+	//			user.email)
+	//	}
+}
+
+
