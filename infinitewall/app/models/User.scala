310de270557dd2330604dc1f9835690165f5a88c
kindone <jradoo@gmail.com>
2012-06-10 21:53:12 +0900

    Basic user signup/signin page with twt bootstrap

--- infinitewall/app/models/User.scala
+++ infinitewall/app/models/User.scala
@@ -8,53 +8,50 @@ import play.api.Logger
 import utils.Mailer
 import Permission._
 
-
-
 object User {
 	def findById(id: Long): Option[User] = {
 		inTransaction {
-			InfiniteWallSchema.users.lookup(id)	
+			InfiniteWallSchema.users.lookup(id)
 		}
 	}
-	
-	def authenticate(username: String, password: String):Option[User] = {
-		inTransaction {			
-			
+
+	def authenticate(email: String, password: String): Option[User] = {
+		inTransaction {
+
 			from(InfiniteWallSchema.users)(
-					user => where(user.username === username) select(user)
+				user => where(user.email === email) select (user)
 			).headOption.flatMap { user =>
-				if(BCrypt.checkpw(password, user.hashedPW))
-					Some(user)
-				else
-					None
-			}
+					if (BCrypt.checkpw(password, user.hashedPW))
+						Some(user)
+					else
+						None
+				}
 		}
 	}
-	
-	def signup(username: String, email:String, password: String): Option[User] = {
-		inTransaction {				
-			
-			if(from(InfiniteWallSchema.users)(
-					user => where(user.username === username) select(user)
-			).isEmpty)  {
-				
-				val newUser:User = InfiniteWallSchema.users.insert(new User(username, email, hashedPW(password), Permission.NormalUser))
+
+	def signup(email: String, password: String): Option[User] = {
+		inTransaction {
+
+			if (from(InfiniteWallSchema.users)(
+				user => where(user.email === email) select (user)
+			).isEmpty) {
+
+				val newUser: User = InfiniteWallSchema.users.insert(new User(email, hashedPW(password), Permission.NormalUser))
 				Mailer.sendVerification(newUser)
-				Some(newUser)			
+				Some(newUser)
 			}
-			else 
+			else
 				None
 		}
 	}
-	
-	private def hashedPW(pw:String) = BCrypt.hashpw(pw, BCrypt.gensalt(12))
-	
-	
+
+	private def hashedPW(pw: String) = BCrypt.hashpw(pw, BCrypt.gensalt(12))
+
 }
 
-class User(val username: String, val email:String, val hashedPW:String, val permission:Permission) extends KeyedEntity[Long] {
-	val id: Long = 0 
-//	lazy val pictures: OneToMany[PictureRef] = OchazukeSchema.userToPicture.left(this)
+class User(val email: String, val hashedPW: String, val permission: Permission) extends KeyedEntity[Long] {
+	val id: Long = 0
+	//	lazy val pictures: OneToMany[PictureRef] = OchazukeSchema.userToPicture.left(this)
 
 }
 
