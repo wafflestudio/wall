a80b502ce24c36d4bed01bb2502df3dfd316762e
Won-Wook Hong <jradoo@gmail.com>
2012-08-06 00:05:28 +0900

    temp

---
+++ infinitewall/app/assets/javascripts/test.js
@@ -0,0 +1,9 @@
+// The test
+
+require("lib");
+
+function showSum(first, second) {
+    alert(require("lib").sum(first, second));
+}
+
+showSum([2,3], 4);
--- infinitewall/project/Build.scala
+++ infinitewall/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
 	val appName = "Infinite Wall"
-	val appVersion = "1.0-SNAPSHOT"
+	val appVersion = "1.0-M1"
 
 	val appDependencies = Seq(
 		// Add your project dependencies here,
