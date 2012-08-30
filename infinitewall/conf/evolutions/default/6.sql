111f957ef7362fdb1907a8c2eb0b393f91c21bd3
Won-Wook Hong <jradoo@gmail.com>
2012-08-30 22:57:42 +0900

    Added group model/schema

--- infinitewall/conf/evolutions/default/6.sql
+++ infinitewall/conf/evolutions/default/6.sql
@@ -2,12 +2,12 @@
 
 # --- !Ups
 
-insert into User values (
+insert into User (id, email, hashedpw, permission) values (
 	(select next value for user_seq),
 	'wall@wall.com', '$2a$12$Sn0vi8ttwkzUFz9uKZkFUuklAYWLzGwlYgDRrPWXGaTBV1IM.josa', 2
 );
 
-insert into Wall values (
+insert into Wall (id, name, user_id, is_reference) values (
 	(select next value for wall_seq),
 	'First wall', (select id from User where email='wall@wall.com'), 0
 );
--- infinitewall/conf/evolutions/default/8.sql
+++ infinitewall/conf/evolutions/default/8.sql
@@ -8,7 +8,17 @@ create table UserGroup (
 	primary key (id)
 );
 
+create table UserInGroup (
+	group_id bigint(20) not null,  
+	user_id bigint(20) not null,
+  constraint fk_useringroup_user foreign key (user_id) references User (id),
+  constraint fk_useringroup_group foreign key (group_id) references UserGroup (id)
+);
+
+create index idx_useringroup_user_1 on UserInGroup (user_id);
+create index idx_useringroup_group_1 on UserInGroup (group_id);
 
 # --- !Downs
 
-drop table UserGroup
+drop table UserInGroup;
+drop table UserGroup;
