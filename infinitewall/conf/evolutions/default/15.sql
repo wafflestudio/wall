6299ce703359bc2339c835b53e409c193db6e602
Won-Wook Hong <jradoo@gmail.com>
2013-01-27 22:18:50 +0900

    fix evolution 15.sql for updating null values

--- infinitewall/conf/evolutions/default/15.sql
+++ infinitewall/conf/evolutions/default/15.sql
@@ -2,10 +2,12 @@
 
 # --- !Ups
 
-alter table User alter column nickname varchar(255) default '' NOT NULL;
 update User set nickname = 'infiniteWall' where email = 'wall@wall.com';
+update User set nickname = '' where nickname = null;
+alter table User alter column nickname varchar(255) default '' NOT NULL;
+
 
 # --- !Downs
 
+alter table User alter column nickname varchar(255);
 update User set nickname = '' where email = 'wall@wall.com';
-alter table alter User column nickname varchar(255);
