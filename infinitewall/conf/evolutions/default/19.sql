32cbfd0c9ea76f1bf6d447e030f3c569ee930aa5
Won-Wook Hong <jradoo@gmail.com>
2013-03-28 11:03:33 +0900

    add additional measure when there exist missing sheet links

--- infinitewall/conf/evolutions/default/19.sql
+++ infinitewall/conf/evolutions/default/19.sql
@@ -1,7 +1,7 @@
 # correct foreign key constraints for sheetlinks
 
 # --- !Ups
-
+delete from SheetLink where from_id NOT IN (select id from Sheet) or to_id NOT IN (select id from Sheet);
 ALTER TABLE SheetLink add constraint fk_sheetlink_from_id foreign key (from_id) references Sheet (id) on delete cascade;
 
 ALTER TABLE SheetLink add constraint fk_sheetlink_to_id foreign key (to_id) references Sheet (id) on delete cascade;
